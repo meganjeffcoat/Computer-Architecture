ddbc05d35d07a6bf2415f7e607dbcfc417112a98
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-15 15:03:50 -0500

    class notes added

--- simple.py
+++ simple.py
@@ -7,28 +7,51 @@ SAVE           = 4  # SAVE VALUE INTO REGISTER
 PRINT_REGISTER = 5
 ADD            = 6
 
-memory = [
-    PRINT_BEEJ,
-    SAVE,  # SAVE 65 into R2
-    65,
-    2,
-    SAVE,  # Save 20 into R3
-    20,
-    3,
-    ADD,   # Add R2 + R3 = 65 + 20, store in R2
-    2,
-    3,
-    PRINT_REGISTER,
-    2,
-    HALT,
-  ]
+
+# 256 bytes of memory
+memory = [0] * 16
+
+# Create 8 registers, 1 byte each
+register = [0] * 8
+
 
 pc = 0
-
 running = True
 
-# Create 8 registers
-register = [0] * 8
+
+def load_memory(filename):
+    try:
+        address = 0
+
+        with open(filename) as f:
+            for line in f:
+                # Process comments:
+                # Ignore anything after a # symbol
+                comment_split = line.split("#")
+
+                # Convert any numbers from binary strings to integers
+                num = comment_split[0].strip()
+                try:
+                    val = int(num)
+                except ValueError:
+                    continue
+
+                memory[address] = val
+                address += 1
+                # print(f"{val:08b}: {val:d}")
+
+    except FileNotFoundError:
+        print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+        sys.exit(2)
+
+
+
+if len(sys.argv) != 2:
+    print("usage: simple.py <filename>", file=sys.stderr)
+    sys.exit(1)
+
+load_memory(sys.argv[1])
+
 
 while running:
     # Do stuff
@@ -63,7 +86,7 @@ while running:
         reg_b = memory[pc+2]   # Get the 2nd register index from 2nd arg
         register[reg_a] += register[reg_b] # Add registers, store in reg_a
         pc += 3
-        
+
     else:
         print(f"Unknown instruction: {command}")
         sys.exit(1)
